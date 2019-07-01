5333e0c1e3c357b3c4ebf72d49743b1dee8bfc24
Anton Sorokoumov <sorokoumov.anton@gmail.com>
2019-07-01 15:19:12 +0300

    Add metrika

--- collector/views.py
+++ collector/views.py
@@ -1,5 +1,10 @@
+# -*- coding: utf-8 -*-
+
 from django.shortcuts import render
 from collector.models import Order, Bid
+import requests
+import json
+import datetime
 
 # Create your views here.
 
@@ -9,8 +14,10 @@ def order(request, number):
     if request.method == "POST":
         bid = Bid(order_id=order, name=request.POST['name'], phone=request.POST['phone'], comment=request.POST['comment'])
         bid.save()
+        write_event('order_taken', order.number)
         return render(request, 'collector/order_taken.html', {'order': order})
     else:
+        write_event('order_open', order.number)
         return render(request, 'collector/order.html', {'order': order})
 
 
@@ -24,3 +31,24 @@ def error500(request, *args, **kwargs):
 
 def in_development(request):
     return render(request, 'technical/in_development.html')
+
+
+def write_event(event, order_id):
+
+    url = 'https://metrika.trucker.group/events'
+    headers = {'content-type': 'application/json;charset=UTF-8'}
+    data_raw = '{"events": [' \
+               '{' \
+               '"user_id": -1,' \
+               '"tags": ["bidok"],' \
+               '"name": "'+event+'",' \
+               '"happened_at": "'+str(datetime.datetime.now())+'",' \
+               '"data": {"order": '+order_id+'}' \
+               '}' \
+               ']}'
+
+    print (data_raw)
+    data = json.loads(data_raw)
+    print (data)
+    response = requests.post(url, data=json.dumps(data), headers=headers)
+
